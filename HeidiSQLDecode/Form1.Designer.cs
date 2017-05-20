4632150b8a9574b730a6df93ef1264031771021a
Elektrik <elektrik1@gmail.com>
2017-05-20 18:00:23 +0300

    First commit

---
+++ HeidiSQLDecode/Form1.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace HeidiSQLDecode {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.decoded = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.input = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.decoded_file = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.decoded_registry = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(729, 404);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.tableLayoutPanel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(721, 378);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Single";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.Location = new System.Drawing.Point(410, 8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(168, 28);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.decoded);
+            this.groupBox2.Location = new System.Drawing.Point(3, 214);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(709, 155);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Decoded:";
+            // 
+            // decoded
+            // 
+            this.decoded.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.decoded.Location = new System.Drawing.Point(23, 19);
+            this.decoded.Multiline = true;
+            this.decoded.Name = "decoded";
+            this.decoded.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.decoded.Size = new System.Drawing.Size(665, 119);
+            this.decoded.TabIndex = 1;
+            this.decoded.WordWrap = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.input);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(709, 155);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input (one password per line):";
+            // 
+            // input
+            // 
+            this.input.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.input.Location = new System.Drawing.Point(23, 23);
+            this.input.Multiline = true;
+            this.input.Name = "input";
+            this.input.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.input.Size = new System.Drawing.Size(665, 112);
+            this.input.TabIndex = 0;
+            this.input.WordWrap = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Location = new System.Drawing.Point(157, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Decode";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.groupBox3);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(721, 354);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Import";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(626, 7);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.decoded_file);
+            this.groupBox3.Location = new System.Drawing.Point(24, 36);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(677, 302);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Decoded:";
+            // 
+            // decoded_file
+            // 
+            this.decoded_file.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.decoded_file.Location = new System.Drawing.Point(23, 19);
+            this.decoded_file.Multiline = true;
+            this.decoded_file.Name = "decoded_file";
+            this.decoded_file.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.decoded_file.Size = new System.Drawing.Size(633, 265);
+            this.decoded_file.TabIndex = 1;
+            this.decoded_file.WordWrap = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(9, 7);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Open file";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.button5);
+            this.tabPage3.Controls.Add(this.groupBox4);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(721, 354);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "From registry";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 404);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(729, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox4.Controls.Add(this.decoded_registry);
+            this.groupBox4.Location = new System.Drawing.Point(24, 36);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(677, 302);
+            this.groupBox4.TabIndex = 6;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Decoded:";
+            // 
+            // decoded_registry
+            // 
+            this.decoded_registry.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.decoded_registry.Location = new System.Drawing.Point(23, 19);
+            this.decoded_registry.Multiline = true;
+            this.decoded_registry.Name = "decoded_registry";
+            this.decoded_registry.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.decoded_registry.Size = new System.Drawing.Size(633, 265);
+            this.decoded_registry.TabIndex = 1;
+            this.decoded_registry.WordWrap = false;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(9, 7);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Refresh";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox2, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(715, 372);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 164);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(709, 44);
+            this.panel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 426);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.statusStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(745, 465);
+            this.Name = "Form1";
+            this.Text = "HeidiSQL Decoder";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox decoded;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox decoded_file;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox decoded_registry;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ HeidiSQLDecode/Form1.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HeidiSQLDecode {
+    public partial class Form1 : Form {
+
+        private bool registry_loaded = false;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e) {
+            string[] lines = input.Text.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+
+            if (lines.Length > 0) {
+                decoded.Clear();
+
+                foreach (String line in lines) {
+                    decoded.AppendText(decode(line) + System.Environment.NewLine);
+                }
+            }
+           
+
+        }
+
+        private String decode(String input) {
+            String err = "*COULD NOT DECODE*";
+            var str = "";
+            String hex = input.Trim();
+            if (String.IsNullOrEmpty(hex)) {
+                return err;
+            }
+            int shift;
+            if(!int.TryParse(hex.Substring(hex.Length - 1, 1), out shift)) {
+                return err;
+            }
+            hex = hex.Substring(0, hex.Length - 1);
+            for (var i = 0; i < hex.Length; i += 2) {
+                int tempval;
+                try {
+                    if (!int.TryParse(hex.Substring(i, 2), System.Globalization.NumberStyles.HexNumber, CultureInfo.CurrentCulture, out tempval)) {
+                        return err;
+                    }
+                } catch (ArgumentOutOfRangeException) {
+                    return err;
+                }
+                str += (char)(tempval - shift);
+            }
+            return (String.IsNullOrEmpty(str) ? "*EMPTY*" : str);
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            input.Clear();
+            decoded.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "HeidiSQL exported file (*.txt)|*.txt";
+            dlg.CheckFileExists = true;
+            dlg.ValidateNames = true;
+            dlg.Multiselect = false;
+            dlg.Title = "Please select exported file";
+
+            if(dlg.ShowDialog() == DialogResult.OK) {
+
+                decoded_file.Clear();
+
+                
+                using (StreamReader sr = new StreamReader(dlg.FileName)) {
+                    String line = sr.ReadToEnd();
+                    
+
+                    try {
+                        Regex regexObj = new Regex(@"(?m:^)Servers\\(.+)\\Password(.+)<\|\|\|>([A-Z0-9]+)");
+
+                        foreach (Match ItemMatch in regexObj.Matches(line)) {
+                            decoded_file.AppendText(ItemMatch.Groups[1] + " - " + ItemMatch.Groups[3] + " - " + decode(ItemMatch.Groups[3].Value) + System.Environment.NewLine);
+                        }
+
+                    } catch (ArgumentException) {
+                        // Syntax error in the regular expression
+                    }
+
+      
+                }
+
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e) {
+            decoded_file.Clear();
+        }
+
+        private void load_registry() {
+            RegistryKey registryKey = Registry.CurrentUser.OpenSubKey("Software\\HeidiSQL\\Servers");
+
+            GetSubKeys(registryKey);
+
+        }
+
+        private void GetSubKeys(RegistryKey SubKey) {
+            foreach (string sub in SubKey.GetSubKeyNames()) {
+                
+                RegistryKey local = Registry.CurrentUser.OpenSubKey("Software\\HeidiSQL\\Servers");
+                local = SubKey.OpenSubKey(sub, true);
+                if (local.GetValueNames().Contains("Password")) {
+                    decoded_registry.AppendText(sub + " - " + local.GetValue("Password") + " - " + decode(local.GetValue("Password").ToString()) + System.Environment.NewLine);
+                }
+                GetSubKeys(local);
+            }
+        }
+
+        private void tabControl1_TabIndexChanged(object sender, EventArgs e) {
+            if(!registry_loaded && tabControl1.TabIndex == 2) {
+                load_registry();
+                registry_loaded = true;
+            }
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e) {
+            if (!registry_loaded && tabControl1.SelectedIndex == 2) {
+                load_registry();
+                registry_loaded = true;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e) {
+            decoded_registry.Clear();
+            load_registry();
+        }
+    }
+}
